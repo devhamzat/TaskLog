4c84cf3733417471cf1fc392fdf5751a9be221f0
devhamzat <ayomidelegbede@gmail.com>
2024-08-22 14:28:13 +0100

    untracked a file

--- src/main/java/org/hae/tasklogue/exceptions/ErrorHandler.java
+++ src/main/java/org/hae/tasklogue/exceptions/ErrorHandler.java
@@ -1,7 +1,7 @@
 package org.hae.tasklogue.exceptions;
 
+import jakarta.mail.MessagingException;
 import org.hae.tasklogue.exceptions.errors.*;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.hae.tasklogue.utils.enums.ErrorCodes.*;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @ControllerAdvice
 @RestController
 public class ErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(AccountExist.class)
     public ResponseEntity<ApplicationError> handleAccountExist(AccountExist exist) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+        return ResponseEntity.status(UNAUTHORIZED).body(
                 ApplicationError.builder()
                         .code(ACCOUNT_EXIST.getCode())
                         .description(ACCOUNT_EXIST.getDescription())
@@ -26,7 +28,7 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmptyRequiredFields.class)
     public ResponseEntity<ApplicationError> handleEmptyRequiredFields(EmptyRequiredFields emptyRequiredFields) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+        return ResponseEntity.status(UNAUTHORIZED).body(
                 ApplicationError.builder()
                         .code(EMPTY_REQUIRED_FIELDS.getCode())
                         .description(EMPTY_REQUIRED_FIELDS.getDescription())
@@ -37,7 +39,7 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ActivationCodeExpired.class)
     public ResponseEntity<ApplicationError> handleActivationCodeExpired(ActivationCodeExpired activationCodeExpired) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+        return ResponseEntity.status(UNAUTHORIZED).body(
                 ApplicationError.builder()
                         .code(ACTIVATION_CODE_EXPIRED.getCode())
                         .description(ACTIVATION_CODE_EXPIRED.getDescription())
@@ -50,7 +52,7 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ApplicationError> handleAccountLockedException(LockedException lockedException) {
 
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+        return ResponseEntity.status(UNAUTHORIZED).body(
                 ApplicationError.builder()
                         .code(ACCOUNT_LOCKED.getCode())
                         .description(ACCOUNT_LOCKED.getDescription())
@@ -63,11 +65,22 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ApplicationError> handleAccountDisabledException(AccountDisabled accountDisabled) {
 
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+        return ResponseEntity.status(UNAUTHORIZED).body(
                 ApplicationError.builder()
                         .code(ACCOUNT_DISABLED.getCode())
                         .description(ACCOUNT_DISABLED.getDescription())
                         .message(accountDisabled.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<ApplicationError> handleMessagingException(MessagingException exception) {
+
+
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(
+                ApplicationError.builder()
+                        .message(exception.getMessage())
                         .build()
         );
     }
